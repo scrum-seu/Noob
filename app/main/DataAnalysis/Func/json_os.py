d56086ae817d6d6fac82323799b4bd67f49ef891
Z_YX <zhyxzhll@foxmail.com>
2019-03-07 10:11:48 +0800

    增加了代码可读性

--- app/main/DataAnalysis/Func/json_os.py
+++ app/main/DataAnalysis/Func/json_os.py
@@ -4,6 +4,7 @@
 import json
 
 
+# 将数据写入.json文件，保证算法效率
 def write_to_json(prefs):
-    with open('../app/main/static/prefs.json', 'w', encoding='utf-8') as file:
+    with open('static/js/prefs.json', 'w', encoding='utf-8') as file:
         json.dump(prefs, file, indent=4)
