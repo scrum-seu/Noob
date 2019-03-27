81d20c348a6febb7734f79926e155b499a77d80c
Ye Shuai <ysssgdhr@icloud.com>
2019-03-27 11:14:25 +0800

    删除重复函数

--- app/main/mainView.py
+++ app/main/mainView.py
@@ -854,45 +854,6 @@ def face_login():
         return None
 
 
-@app.route("/get_like_info", methods=["GET", "POST"])
-def get_like_info():
-    """
-    获取用户喜欢的所有商品信息
-    :return: res_dict = {"data": [{good_id:
-                                   good_name:
-                                   ...:
-                                   }],...}
-             失败返回： None
-    """
-    if request.method == "POST":
-        try:
-            req_data = request.get_json()
-            user_id = req_data["user_id"]
-        except Exception as e:
-            print(e)
-            print(
-                "request the function paramters of get_like_info failed!"
-            )
-            return None
-        try:
-            res_dict = {}
-            res_list = []
-            goods = get_session().query(Goods).join(Like_info, Like_info.good_id == Goods.good_id).\
-                filter(Like_info.user_id == user_id).all()  # 获取用户喜欢的所有商品信息
-            for good in goods:
-                item_dict = good.getinfo()
-                # 添加一个商品信息及推荐类型
-                res_list.append(item_dict)
-            res_dict["data"] = res_list
-            # 返回字符串化结果字典
-            return json.dumps(res_dict, cls=DecimalEncoder)
-            # return json.dumps(res_dict)
-        except Exception as e:
-            print("error: {}\n failed to get SQL of like_info!".format(e))
-            return None
-    else:
-        return None
-
 
 # =========================================================================
 # ================================data calculation=========================
